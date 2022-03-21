5b744811f81dccbdc6f6bbfc24e5d322d9e9a1a2
Your Name <you@example.com>
2022-03-21 11:24:35 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { InfoComponent } from './info/info.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { BannerComponent } from './banner/banner.component';
+import {SidabarComponent} from'./sidabar/sidabar.component';
+const routes: Routes = [
+  {path:'', redirectTo:'home',pathMatch: 'full'},
+  {path:'sidabar',component: SidabarComponent},
+  {path:'home',component: HomeComponent},
+  {path:'info',component: InfoComponent},
+  {path:'aboutus',component: AboutusComponent},
+  {path:'banner',component: BannerComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidabarComponent } from './sidabar/sidabar.component';
+import { InfoComponent } from './info/info.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { HomeComponent } from './home/home.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidabarComponent,
+    InfoComponent,
+    AboutusComponent,
+    HomeComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
